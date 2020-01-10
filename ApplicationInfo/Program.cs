090c8a84300b2d97ca59bae2a2324d95f9077369
Kees Hiemstra <chi@xs4all.nl>
2020-01-10 14:02:05 +0100

    Application Informatie

---
+++ ApplicationInfo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInfo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+      Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+      Console.WriteLine($"Version: {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()}");
+
+      Console.WriteLine("\nPress any key...");
+      Console.ReadKey();
+    }
+  }
+}
