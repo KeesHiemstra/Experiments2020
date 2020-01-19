274ce3ed955763303b3158a1174a17a570975c76
Kees Hiemstra <chi@xs4all.nl>
2020-01-19 17:23:48 +0100

    Develop HistoryWindow

--- Graph1/ViewModels/MainViewModel.cs
+++ Graph1/ViewModels/MainViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CHi.Extensions;
+using Graph1.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +15,8 @@ namespace Graph1.ViewModels
 
     MainWindow MainView;
 
+    public List<DailyReport> Daily { get; set; } = new List<DailyReport>();
+
     public MainViewModel(MainWindow mainView)
     {
 
@@ -18,5 +24,21 @@ namespace Graph1.ViewModels
 
     }
 
+    internal void PaintGraphic()
+    {
+
+      using (StreamReader stream = File.OpenText("%OneDrive%\\Tmp\\Data.json".TranslatePath()))
+      {
+        string json = stream.ReadToEnd();
+        Daily = JsonConvert.DeserializeObject<List<DailyReport>>(json);
+      }
+      CreateTemperatureGraphic(Daily);
+
+    }
+
+    private void CreateTemperatureGraphic(List<DailyReport> daily)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
