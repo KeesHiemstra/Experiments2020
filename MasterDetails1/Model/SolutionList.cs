2716675a6264e9536b30ccdd66a58f27a5aedf8c
Kees Hiemstra <chi@xs4all.nl>
2020-03-10 17:03:23 +0100

    This experiment uses a master-detail screen without addition code for the navigation.

---
+++ MasterDetails1/Model/SolutionList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MasterDetails1.Model
+{
+  class SolutionList
+  {
+
+    private ObservableCollection<Solution> solutions;
+
+    public ObservableCollection<Solution> Solutions
+    {
+      get => solutions;
+    }
+
+    //This line uses 1 MB more.
+    //public ObservableCollection<Solution> Solutions { get; set; } = new ObservableCollection<Solution>();
+
+    public SolutionList()
+    {
+      solutions = new ObservableCollection<Solution>();
+
+      Solution solution = new Solution() { Name = "Banking", Folder = "C:\\Dev\\Banking" };
+      solution.Projects.Add(new Project() { Name = "Banking" });
+      solution.Projects.Add(new Project() { Name = "Develop MissedTalliesView" });
+      Solutions.Add(solution);
+
+      solution = new Solution() { Name = "Tools", Folder = "C:\\Dev\\Tools" };
+      solution.Projects.Add(new Project() { Name = "CopyLogonBackgroundPicture" });
+      solution.Projects.Add(new Project() { Name = "MedicineStock" });
+      Solutions.Add(solution);
+
+    }
+
+  }
+}
---
+++ MasterDetails1/README.md
@@ -0,0 +1,7 @@
+﻿# Display Master-Detail
+
+This experiment uses a master-detail screen without addition code for the navigation.
+
+It doesn't make to public for the model classes.
+
+
