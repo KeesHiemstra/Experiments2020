a746d2c8d4fa7b99c27c884ca3755016cc6b188c
Kees Hiemstra <chi@xs4all.nl>
2020-03-10 12:04:33 +0100

    WIP for several experiments

---
+++ Graph2/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CHi.Extensions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using WeatherDemon.Models;
+
+namespace Graph2.ViewModels
+{
+  public class MainViewModel
+  {
+    #region [ Fields ]
+
+    private MainWindow MainView;
+
+    #endregion [ Fields ]
+
+    #region [ Properties ]
+
+    public List<DayWeather> TodayWeather { get; set; }
+
+    #endregion
+
+    #region [ Construction ]
+
+    public MainViewModel(MainWindow mainView)
+    {
+      MainView = mainView;
+      PaintTemperature();
+    }
+
+    #endregion [ Construction ]
+
+    private void PaintTemperature()
+    {
+      string jsonPath = "%OneDrive%\\Data\\DailyWeather\\DayWeather.json".TranslatePath(); 
+      using (StreamReader stream = File.OpenText(jsonPath))
+      {
+        string json = stream.ReadToEnd();
+        TodayWeather = JsonConvert.DeserializeObject <List<DayWeather>>(json);
+			}
+    }
+  }
+}
---
+++ ToDone/README.md
@@ -0,0 +1,9 @@
+﻿# ToDone
+
+A to-do list is more than that only. Every day, week, 
+month or year is also a leading event.
+
+### Wishes
+
+- [ ] A to-do can be related to certain day, like there is coffee-meeting on every Thursday. Not every week is there a need, sometimes there is reason. It can be helpful as a reminder thing.
+- [ ] A to-do can have a related action.
