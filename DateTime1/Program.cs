1490ddff3aa7b5137e7c004e46043c4de5b2000b
Kees Hiemstra <chi@xs4all.nl>
2020-08-02 19:31:59 +0200

    Testing with Debug.Assert(dateTime.IsDaylightSavingTime(), "Daylight saving time");

--- DateTime1/Program.cs
+++ DateTime1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace DateTime1
 {
@@ -20,6 +17,7 @@ namespace DateTime1
 
       for (int i = 0; i < 7; i++)
       {
+        Debug.Assert(dateTime.IsDaylightSavingTime(), "Daylight saving time");
         Console.WriteLine(dateTime.AddDays(i).ToLocalTime());
       }
 
