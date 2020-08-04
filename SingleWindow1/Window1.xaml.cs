6eb655c5048dccb5d143eec4aad981144b0c10da
Kees Hiemstra <chi@xs4all.nl>
2020-08-04 12:48:47 +0200

    Add SingleWindow1

---
+++ SingleWindow1/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SingleWindow1
+{
+  /// <summary>
+  /// Interaction logic for Window1.xaml
+  /// </summary>
+  public partial class Window1 : Window
+  {
+    Window _Parent;
+    public Window1(Window parent)
+    {
+      InitializeComponent();
+      _Parent = parent;
+      _Parent.Hide();
+    }
+
+    private void CloseWindow(object sender, RoutedEventArgs e)
+    {
+      _Parent.Show();
+      Close();
+    }
+  }
+}
