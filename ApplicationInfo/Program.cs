264fc93bafd111e33263a331c72b630a6bf03ca1
Kees Hiemstra <chi@xs4all.nl>
2020-10-22 06:53:51 +0200

    WIP

--- ApplicationInfo/Program.cs
+++ ApplicationInfo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +12,26 @@ namespace ApplicationInfo
   {
     static void Main(string[] args)
     {
-      Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
-      Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
-      Console.WriteLine($"Version: {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()}");
+      var Name = Assembly.GetExecutingAssembly().GetName().Name;
+      var CodeBase = Assembly.GetExecutingAssembly().GetName().CodeBase;
+      Console.WriteLine($"Name: {Name}");
+      Console.WriteLine($"CodeBase: {CodeBase}");
+			Console.WriteLine($"ContentType: {Assembly.GetExecutingAssembly().GetName().ContentType}");
+      Console.WriteLine($"FullName: {Assembly.GetExecutingAssembly().GetName().FullName}");
+      Console.WriteLine($"VersionCompatibility: {Assembly.GetExecutingAssembly().GetName().VersionCompatibility}");
+      Console.WriteLine($"Location: {Assembly.GetExecutingAssembly().Location}");
+      Console.WriteLine($"ReflectionOnly: {Assembly.GetExecutingAssembly().ReflectionOnly}");
+      Console.WriteLine($"");
+      Console.WriteLine($"Version: {Assembly.GetExecutingAssembly().GetName().Version.ToString()}");
+
+
+			Console.WriteLine();
+      string file = Assembly.GetExecutingAssembly().Location;
+      Console.WriteLine($"Location: {file.Remove(file.LastIndexOf('\\'))}");
+
+			Console.WriteLine();
+      var path = Directory.GetCurrentDirectory();
+			Console.WriteLine($"Current path: {path}");
 
       Console.WriteLine("\nPress any key...");
       Console.ReadKey();
